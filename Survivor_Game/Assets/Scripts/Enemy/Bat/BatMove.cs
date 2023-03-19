e8f4ef8b1ac2d0b34c7ba3b064920ad7fefd27de
Nhat Anh <tanhatanh76@gmail.com>
2023-03-20 01:56:19 +0700

    anhtn_20/3/2023

---
+++ Survivor_Game/Assets/Scripts/Enemy/Bat/BatMove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject walkers = GameObject.FindGameObjectWithTag("Player");
+
+        Vector2 closestWalkerDirection = Vector2.zero;
+        float closestDistance = Mathf.Infinity;
+
+        float distance = Vector2.Distance(transform.position, walkers.transform.position);
+        if (distance < closestDistance && distance <= 10f)
+        {
+            closestDistance = distance;
+            closestWalkerDirection = (walkers.transform.position - transform.position).normalized;
+        }
+        if (closestDistance <= 10f)
+        {
+            float angle = Mathf.Atan2(closestWalkerDirection.y, closestWalkerDirection.x) * Mathf.Rad2Deg - 90f;
+            Quaternion lookRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+}
