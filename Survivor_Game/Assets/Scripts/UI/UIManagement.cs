44f7f280f9c703be74c69c81e52a7d72594290c6
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2023-03-21 16:29:52 +0700

    Vinhlq_61
    Add CoolDown for bullet 4

--- Survivor_Game/Assets/Scripts/UI/UIManagement.cs
+++ Survivor_Game/Assets/Scripts/UI/UIManagement.cs
@@ -2,9 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
+using Assets.Scripts.Model;
+
+using Unity.VisualScripting;
+
+using UnityEditor.Timeline.Actions;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.U2D;
 using UnityEngine.UI;
 
 public class UIManagement : MonoBehaviour
@@ -18,41 +25,87 @@ public class UIManagement : MonoBehaviour
     public Button Bullet4;
     public Button MainMenu;
 
+    public Text txtCoolDown;
+
     private int scoreValue;
+    private int coolDown;
+    private int check;
+
+
+    private PlayerController playerController;
     // Start is called before the first frame update
 
     public void Start()
     {
-
+        playerController = FindObjectOfType<PlayerController>();
         Bullet1.onClick.AddListener(ChangeSkill);
         Bullet2.onClick.AddListener(ChangeSkill);
         Bullet3.onClick.AddListener(ChangeSkill);
         Bullet4.onClick.AddListener(ChangeSkill);
         MainMenu.onClick.AddListener(MainMenuButton);
         scoreValue = 0;
-
     }
 
     private void ChangeSkill()
     {
-        PlayerController playerController = FindObjectOfType<PlayerController>();
+        
         string bullet = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
         switch (bullet)
         {
             case "Bullet 1":
+                Bullet2.GetComponent<Button>().interactable = true;
+                Bullet3.GetComponent<Button>().interactable = true;
+                Bullet1.GetComponent<Button>().interactable = false;
+
                 playerController.ChangeWeapon(1);
                 break;
             case "Bullet 2":
+                Bullet3.GetComponent<Button>().interactable = true;
+                Bullet1.GetComponent<Button>().interactable = true;
+                Bullet2.GetComponent<Button>().interactable = false;
+
                 playerController.ChangeWeapon(2);
                 break;
             case "Bullet 3":
+                Bullet2.GetComponent<Button>().interactable = true;
+                Bullet1.GetComponent<Button>().interactable = true;
+                Bullet3.GetComponent<Button>().interactable = false;
+
                 playerController.ChangeWeapon(3);
                 break;
             case "Bullet 4":
+                Bullet4.GetComponent<Button>().interactable = false;
+                Bullet4.GetComponent<Image>().color = Color.red;   
+                Bullet2.GetComponent<Button>().interactable = true;
+                Bullet1.GetComponent<Button>().interactable = true;
+                Bullet3.GetComponent<Button>().interactable = true;
                 playerController.ChangeWeapon(4);
+                check = 0;
+                coolDown = 5;
+                InvokeRepeating("RunCoolDown", 1f, 1f);
                 break;
         }
     }
+
+    public void RunCoolDown()
+    {
+        coolDown--;
+        txtCoolDown.text = coolDown.ToString();
+        if (coolDown == 0 && check == 0)
+        {
+            Bullet1.GetComponent<Button>().interactable = false;
+            playerController.ChangeWeapon(1);
+            Bullet4.GetComponent<Image>().color = Color.white;
+            check++;
+            coolDown = 15;
+        }
+        if(coolDown == 0 && check == 1)
+        {
+            Bullet4.GetComponent<Button>().interactable = true;
+            CancelInvoke();
+        }
+    }
+
 
     public void SetCoreText(string text)
     {
