2cf0142cc09716f23c0859c4de3a7e82f2166434
Nhat Anh <tanhatanh76@gmail.com>
2023-03-21 16:50:42 +0700

    anhtn_suacollision_21/3

--- Survivor_Game/Assets/Scripts/Enemy/Bat/BatColiision.cs
+++ Survivor_Game/Assets/Scripts/Enemy/Bat/BatColiision.cs
@@ -5,25 +5,37 @@ using UnityEngine;
 public class BatColiision : MonoBehaviour
 {
     public float damage;
-    public float pushForce = 10f; // Lực đẩy khi va chạm
+    public float pushForce = 10f; // Lực đẩy
 
-    private void OnTriggerEnter2D(Collision2D collision)
+    private void OnTriggerStay2D(Collider2D other)
     {
-        if (collision.gameObject.tag == "Player")
+        if (other.gameObject.name.Equals("Player"))
         {
-            Rigidbody2D playerRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
-            Vector2 pushDirection = collision.contacts[0].normal; // Hướng đẩy
-
-            playerRigidbody.velocity = pushDirection * pushForce; // Đẩy player theo hướng va chạm
-        
-        // Trừ điểm máu của player
-        PlayerInfomation player = collision.gameObject.GetComponent<PlayerInfomation>();
-            if (player.currentHealth > 0)
+            Rigidbody2D playerRigidbody = other.GetComponent<Rigidbody2D>();
+            Rigidbody2D player = other.gameObject.GetComponent<Rigidbody2D>();
+            Vector2 pushDirection = playerRigidbody.transform.position - transform.position; // Xác định hướng đẩy
+            pushDirection.Normalize();
+            if (player != null)
             {
-                float playerLevel = player.GetComponent<PlayerInfomation>().lv;
-                damage = 10f - 0.3f * playerLevel;
-                player.TakeDamage(damage);
+                playerRigidbody.AddForce(pushDirection * pushForce, ForceMode2D.Impulse); // Đẩy player
+                StartCoroutine(StopPlayer(player));
             }
         }
+
+
+        // Trừ điểm máu của player
+        PlayerInfomation player1 = other.gameObject.GetComponent<PlayerInfomation>();
+        if (player1.currentHealth > 0)
+        {
+            float playerLevel = player1.GetComponent<PlayerInfomation>().lv;
+            damage = 10f - 0.3f * playerLevel;
+            player1.TakeDamage(damage);
+        }
+    }
+
+    IEnumerator StopPlayer(Rigidbody2D playerRb)
+    {
+        yield return new WaitForSeconds(0.1f); // Đợi 0.5 giây trước khi dừng player
+        playerRb.velocity = Vector3.zero; // Dừng player
     }
 }
